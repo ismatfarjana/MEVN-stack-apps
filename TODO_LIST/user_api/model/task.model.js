5573fa7622abb68f5105729d0c9e8ab6208c3cba
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2022-10-16 09:15:17 +1100

    add tasks schema

---
+++ TODO_LIST/user_api/model/task.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  name: 'string',
+  description: 'string',
+  createDate: 'date',
+  updateDate: 'date',
+  createdBy: 'string',
+  updateBy: 'string',
+},
+  {
+    timestamps: {
+      createDate: 'created_at',
+      updateDate: 'updated_at'
+    }
+  }
+);
+
+const Task = mongoose.model('tasks', taskSchema);
+
+module.exports = {
+  Task
+}
