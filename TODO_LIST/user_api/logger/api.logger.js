6f12c24523ee61bbc2ce66349cd01d171b8da40e
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2022-10-19 14:19:30 +1100

    ad pine APILogger

--- TODO_LIST/user_api/logger/api.logger.js
+++ TODO_LIST/user_api/logger/api.logger.js
@@ -1,15 +1,36 @@
-const Logger = function () { };
+// Legasy logger
+// const Logger = function () { };
 
-Logger.prototype.info = (logText) => {
-  console.log(`info::${logText}`);
-};
+// Logger.prototype.info = (logText) => {
+//   console.log(`info::${logText}`);
+// };
 
-Logger.prototype.debug = (logText) => {
-  console.log(`debug::${logText}`);
-};
+// Logger.prototype.debug = (logText) => {
+//   console.log(`debug::${logText}`);
+// };
 
-Logger.prototype.error = (logText) => {
-  console.log(`error::${logText}`);
-};
+// Logger.prototype.error = (logText) => {
+//   console.log(`error::${logText}`);
+// };
 
-module.exports = new Logger();
+// module.exports = new Logger();
+
+const pine = require('pine');
+
+const logger = pine();
+
+class APILogger {
+  info(message) {
+    logger.info(message);
+  }
+
+  info(message, data) {
+    logger.info(`${message}   ${undefined != data ? JSON.stringify(data) : ''}`);
+  }
+
+  error(message) {
+    logger.error(message)
+  }
+}
+
+module.exports = new APILogger();
